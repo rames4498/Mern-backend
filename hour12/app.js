d32a7670c81682f7b210a6bcf47213fe7969e4d9
N Ramesh <nagurramesh221b@gmail.com>
2023-05-03 12:26:55 +0530

    new changes

--- hour12/app.js
+++ hour12/app.js
@@ -7,7 +7,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@net-ninja-tuts-del96.mongodb.net/node-tuts";
+const dbURI = "mongodb+srv://sherlockcourses:myDatabase123@cluster0.qg6beu1.mongodb.net/backend-basics";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
